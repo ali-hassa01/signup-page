57e9d1baf4fda827564d958e18b6f8e09529e4d9
ali-hassa01 <aalihassanmughal1@gmail.com>
2026-06-28 23:42:44 +0500

    Add Firebase authentication and login flow
    Added Firebase authentication, signup page, login page, dashboard with sign out, and updated project flow.

--- script.js
+++ script.js
@@ -1,18 +1,72 @@
-document.getElementById("signupForm").addEventListener("submit", function(e){
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  sendPasswordResetEmail
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCF35CQr_6s6nI2W59HEvPWDH1dZOlxgRs",
+  authDomain: "signup-page-2a58f.firebaseapp.com",
+  projectId: "signup-page-2a58f",
+  storageBucket: "signup-page-2a58f.firebasestorage.app",
+  messagingSenderId: "150901596981",
+  appId: "1:150901596981:web:6bdce5df55e35de3ed227c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Email Signup
+document.getElementById("signupForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    alert("Signup Successful!");
-
-    console.log("Email:", email);
-    console.log("Password:", password);
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        alert("Signup Successful!");
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+        alert(error.message);
+    });
 });
-function googleSignup(){
-    alert("Google Sign Up Clicked");
-}
-function forgotPassword(){
-    alert("Password Reset Link Sent");
-}
+
+// Google Signup
+window.googleSignup = function () {
+    const provider = new GoogleAuthProvider();
+
+    signInWithPopup(auth, provider)
+    .then(() => {
+        window.location.href = "dashboard.html";
+    })
+    .catch((error) => {
+        alert(error.message);
+    });
+};
+
+// Forgot Password
+window.forgotPassword = function () {
+    const email = document.getElementById("email").value;
+
+    if (email === "") {
+        alert("Please enter your email first.");
+        return;
+    }
+
+    sendPasswordResetEmail(auth, email)
+    .then(() => {
+        alert("Password reset email sent.");
+    })
+    .catch((error) => {
+        alert(error.message);
+    });
+};
